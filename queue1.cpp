511d9ccda42b4446f65fac8ff1d7d4fbb81f88e9
Sheery <sk9109182@gmail.com>
2024-05-15 15:35:43 +0500

    Update queue1.cpp

--- queue1.cpp
+++ queue1.cpp
@@ -1,75 +1,89 @@
-#include<iostream>
+#include <iostream>
+#include <stdlib.h>
 using namespace std;
 
-# define n 20
-class queue{
-	int* arr;
-	int front;
-	int back;
-	
-	public:
-	queue(){
-		arr= new int[n];
-		front=-1;
-		back=-1;
-	}
-	void push(int x){
-		if(back==n-1){
-			cout<<"Queue overflow"<<endl;
-			return;
-		}
-		back++;
-		arr[back]=x;
-		
-		if(front==-1){
-			front++;
-		}
-	}
-	void pop(){
-		if(front==-1 || front>back){
-			cout<<"No elements in queue"<<endl;
-			return;
-		}
-		front++;
-	}
-	
-	int peek(){
-		if(front==-1 || front>back){
-			cout<<"No elements in queue"<<endl;
-			return-1;
-    }
-	    return arr[front];
-	}
-	bool empty(){
-		if(front==-1 || front>back){
-			cout<<"No elements in queue"<<endl;
-			return true;
-		}
-		return false;
-	}
+struct studentRecord
+{
+    int rollnumber;
+    float gpa;
+    studentRecord* next;
 };
 
-int main(){
-	queue q;
-	q.push(1);
-	q.push(2);
-	q.push(3);
-	q.push(4);
-	
-	cout<<q.peek()<<endl;
-	q.pop();
-	
-	cout<<q.peek()<<endl;
-	q.pop();
-	
-	cout<<q.peek()<<endl;
-	q.pop();
-	
-	cout<<q.peek()<<endl;
-	q.pop();
-	
-	cout<<q.empty()<<endl;
-	
-	return 0;
-	
+void enqueue(studentRecord** front, studentRecord** rear)
+{
+    studentRecord* ptr = (studentRecord*)malloc(sizeof(studentRecord));
+    cout << "Insert Roll Number: ";
+    cin >> ptr->rollnumber;
+    cout << "Insert GPA: ";
+    cin >> ptr->gpa;
+    ptr->next = NULL;
+    if (*front == NULL && *rear == NULL)
+    {
+        *front = *rear = ptr;
+    }
+    else
+    {
+        studentRecord* curr = *rear;
+        *rear = ptr;
+        curr->next = *rear;
+    }
+}
+
+void dequeue(studentRecord** front, studentRecord** rear)
+{
+    studentRecord* curr = *front;
+    if(curr != NULL)
+    {
+        *front = (*front)->next;
+        free(curr);
+        
+    }
+    else
+    {
+        cout << "Queue Underflow" << endl;
+    }
+}
+
+void print(studentRecord** front, studentRecord** rear)
+{
+    studentRecord* curr = *front;
+    while (curr != NULL)
+    {
+        cout << "Roll Number: " << curr->rollnumber << endl;
+        cout << "GPA: " << curr->gpa << endl;
+        cout << "" << endl;
+        curr = curr->next;
+    }
+}
+
+int main()
+{
+    studentRecord* front = NULL;
+    studentRecord* rear = NULL;
+    cout << "List is currently empty" << endl;
+    enqueue(&front, &rear);
+    cout << "Press 1 to push" << endl;
+    cout << "Press 2 to pop" << endl;
+    cout << "Press 3 to print" << endl;
+    cout << "Press 0 to exit" << endl;
+    cout << "Enter your choice: " << "";
+    int select = 1;
+    cin >> select;
+    while (select != 0)
+    {
+        if (select == 1)
+        {
+            enqueue(&front, &rear);
+        }
+        if (select == 2)
+        {
+            dequeue(&front, &rear);
+        }
+        if (select == 3)
+        {
+            print(&front, &rear);
+        }
+        cout << "Enter your choice again: "<< "";
+        cin >> select;
+    }
 }
