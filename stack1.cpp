e3227572002b97bed8d551af5352353a2885c71c
Sheery <sk9109182@gmail.com>
2024-05-15 15:34:56 +0500

    Update stack1.cpp

--- stack1.cpp
+++ stack1.cpp
@@ -1,64 +1,97 @@
-#include<iostream>
+#include <iostream>
+#include <stdlib.h>
 using namespace std;
 
-#define n 100
-
-class stack{
-	int*arr;
-	int top;
-	public:
-	stack(){
-		arr=new int[n];
-		top=-1;
-	}
-	
-	void push(int x){
-		if(top==n-1){
-			cout<<" stack overflow"<<endl;
-			return;
-		}
-		
-		top ++;
-		arr[top]=x;
-		
-	}
-	void pop(){
-		if(top==-1){
-			cout<<"No element to pop"<<endl;
-			return;
-		}
-		top--;
-	}
-	int Top(){
-		if(top==-1){
-		    cout<<"No element in stack"<<endl;
-			return-1;
-		}
-		return arr[top];
-	}
-	
-	bool empty(){
-		return top==-1;
-	}
+struct studentRecord
+{
+    int rollnumber;
+    float gpa;
+    studentRecord* next;
 };
 
-
-
-int main(){
-	
-	stack st;
-	st.push(1);
-	st.push(2);
-	st.push(3);
-	cout<<st.Top()<<endl;
-	st.pop();
-	cout<<st.Top()<<endl;
-	st.pop();
-	st.pop();
-	st.pop();
-	cout<<st.empty()<<endl;
-	
-	return 0;
-		
+void push(studentRecord** top)
+{
+    studentRecord* ptr = (studentRecord*)malloc(sizeof(studentRecord));
+    cout << "Insert Roll Number: ";
+    cin >> ptr->rollnumber;
+    cout << "Insert GPA: ";
+    cin >> ptr->gpa;
+    ptr->next = NULL;
+    if (top == NULL)
+    {
+        *top = ptr;
+    }
+    else
+    {
+        studentRecord* curr = *top;
+        ptr->next = curr;
+        *top = ptr;
+    }
 }
 
+void print(studentRecord** top)
+{
+    studentRecord* curr = *top;
+    while (curr != NULL)
+    {
+        cout << "Roll Number: " << curr->rollnumber << endl;
+        cout << "GPA: " << curr->gpa << endl;
+        cout << "" << endl;
+        curr = curr->next;
+    }
+}
+
+void pop(studentRecord** top)
+{
+	studentRecord* curr = *top;
+	if (curr != NULL)
+	{
+	if (curr->next == NULL)
+	{
+		free(curr);
+		cout << "List is now empty"<< endl;
+		*top = NULL;
+	}
+	else
+	{
+		*top = (*top)->next;
+		free(curr);
+	}
+	}
+	else
+	{
+		cout << "Stack Underflow" << endl;
+	}
+}
+
+int main()
+{
+    studentRecord* top = NULL;
+    cout << "List is currently empty" << endl;
+    push(&top);
+    cout << "Press 1 to push" << endl;
+    cout << "Press 2 to pop" << endl;
+    cout << "Press 3 to print" << endl;
+    cout << "Press 0 to exit" << endl;
+    cout << "Enter your choice: " << "";
+    int select = 1;
+    cin >> select;
+    while (select != 0)
+    {
+        if (select == 1)
+        {
+            push(&top);
+        }
+        if (select == 2)
+        {
+            pop(&top);
+        }
+        if (select == 3)
+        {
+            print(&top);
+        }
+        
+        cout << "Enter your choice again: "<< "";
+        cin >> select;
+    }
+}
