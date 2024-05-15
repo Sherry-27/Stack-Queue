877c20ae64e6a8bbc9095f5ccd5c9d89300218d4
Sheery <sk9109182@gmail.com>
2024-05-15 15:34:04 +0500

    Update stacklinkedlist.cpp

--- stacklinkedlist.cpp
+++ stacklinkedlist.cpp
@@ -1,57 +1,47 @@
 #include <iostream>
 using namespace std;
 
-
-class StackNode {
-public:
+struct StackNode {
     int data;
     StackNode* next;
 };
 
-StackNode* newNode(int data)
-{
+StackNode* newNode(int data) {
     StackNode* stackNode = new StackNode();
     stackNode->data = data;
-    stackNode->next = NULL;
+    stackNode->next = nullptr;
     return stackNode;
 }
 
-int isEmpty(StackNode* root)
-{
+int isEmpty(StackNode* root) {
     return !root;
 }
 
-void push(StackNode** root, int data)
-{
+void push(StackNode** root, int data) {
     StackNode* stackNode = newNode(data);
     stackNode->next = *root;
     *root = stackNode;
     cout << data << " pushed to stack\n";
 }
 
-int pop(StackNode** root)
-{
+int pop(StackNode** root) {
     if (isEmpty(*root))
         return INT_MIN;
     StackNode* temp = *root;
     *root = (*root)->next;
     int popped = temp->data;
-    free(temp);
-
+    delete temp;
     return popped;
 }
 
-int peek(StackNode* root)
-{
+int peek(StackNode* root) {
     if (isEmpty(root))
         return INT_MIN;
     return root->data;
 }
 
-
-int main()
-{
-    StackNode* root = NULL;
+int main() {
+    StackNode* root = nullptr;
 
     push(&root, 10);
     push(&root, 20);
@@ -60,14 +50,11 @@ int main()
     cout << pop(&root) << " popped from stack\n";
 
     cout << "Top element is " << peek(root) << endl;
-    
-    cout <<"Elements present in stack : ";
-     
-    while(!isEmpty(root))
-    {
-        
-        cout << peek(root) <<" ";
-        
+
+    cout << "Elements present in stack: ";
+
+    while (!isEmpty(root)) {
+        cout << peek(root) << " ";
         pop(&root);
     }
 
